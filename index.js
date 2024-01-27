680dc640ed59c668669cc8ef8e99a727ff4a2e4d
Repeatallright <levserg2003@gmail.com>
2024-01-27 13:42:30 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let scene = document.querySelector(".scene");
 let box = document.querySelector(".box");
 let right_header = document.querySelector(".right_header");
-let main_slogan = document.querySelector(".text");
+let main_slogan = document.querySelector(".text2");
 let global_filter = document.querySelector(".global_filter");
 let global_img = document.querySelector(".global_img");
 
@@ -58,8 +58,9 @@ window.onload = () => {
     global_img.style.opacity = "100%";
   }, 2500);
   setTimeout(() => {
-    main_slogan.style.transition = "1s";
-    main_slogan.style.opacity = "80%";
+    // main_slogan.style.transition = "1s";
+    // main_slogan.style.opacity = "80%";
+    sloganEffect();
   }, 3000);
 
   box.style.transform = `
@@ -70,16 +71,51 @@ window.onload = () => {
   scale(${logoSize() * 100}%)
   `;
 
-  console.log(box);
   if (deg % 180 == 0 && deg % 360 != 0)
     box.style.boxShadow = "-5px 5px 15px -4px rgba(0,0,0,0.5)";
   if (deg % 360 == 0) box.style.boxShadow = "5px 5px 15px -4px rgba(0,0,0,0.5)";
 };
-
-console.log("Hellow world");
 
 function logoSize() {
   return (w * settings.logoScale) / elementW;
 }
 
 logoSize();
+
+function sloganEffect() {
+  let main = document.querySelector(".slogan");
+  main.style.opacity = "100%";
+
+  let slogans = [...main.innerHTML];
+  main.innerHTML = "";
+
+  let word = document.createElement("div");
+  word.classList.add("word");
+  slogans.forEach((item, index) => {
+    if (index == 0) {
+      let voi = document.createElement("div");
+      voi.style.display = "none";
+      main.appendChild(voi);
+    }
+    if (item == " ") {
+      let spread = document.createElement("div");
+      spread.classList.add("letter");
+      spread.innerHTML = "&nbsp";
+      main.appendChild(word);
+      main.appendChild(spread);
+      word = document.createElement("div");
+      word.classList.add("word");
+    } else if (item != "") {
+      let letter = document.createElement("div");
+      letter.classList.add("letter");
+      letter.innerHTML = item;
+      setTimeout(() => {
+        letter.style.opacity = "100%";
+      }, index * 80);
+      word.appendChild(letter);
+    }
+    if (index + 1 == slogans.length) {
+      main.appendChild(word);
+    }
+  });
+}
